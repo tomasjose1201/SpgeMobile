eb317142d0cd0d8f7625d6cb56cdab56089bf09e
Tom <tomasjose1201@gmail.com>
2018-05-07 22:17:12 -0300

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -2,68 +2,89 @@
 var $$ = Dom7;
 
 // Framework7 App main instance
-var app  = new Framework7({
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
-  // App root data
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-      // Demo products for Catalog section
-      products: [
-        {
-          id: '1',
-          title: 'Apple iPhone 8',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-        },
-        {
-          id: '2',
-          title: 'Apple iPhone 8 Plus',
-          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-        },
-        {
-          id: '3',
-          title: 'Apple iPhone X',
-          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-        },
-      ]
-    };
-  },
-  // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
+var app = new Framework7({
+    root: '#app', // App root element
+    id: 'io.framework7.testapp', // App bundle ID
+    name: 'Framework7', // App name
+    theme: 'auto', // Automatic theme detection
+    // App root data
+    data: function () {
+        return {
+            user: {
+                firstName: 'John',
+                lastName: 'Doe',
+            },
+            // Demo products for Catalog section
+            products: [
+                {
+                    id: '1',
+                    title: 'Apple iPhone 8',
+                    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+                },
+                {
+                    id: '2',
+                    title: 'Apple iPhone 8 Plus',
+                    description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+                },
+                {
+                    id: '3',
+                    title: 'Apple iPhone X',
+                    description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+                },
+            ]
+        };
     },
-  },
-  // App routes
-  routes: routes,
+    // App root methods
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
+    },
+    // App routes
+    routes: routes,
 });
 
 // Init/Create views
-var homeView = app.views.create('#view-home', {
-  url: '/'
-});
-var catalogView = app.views.create('#view-catalog', {
-  url: '/catalog/'
-});
-var settingsView = app.views.create('#view-settings', {
-  url: '/settings/'
+/*var homeView = app.views.create('#view-home', {
+ url: '/'
+ });
+ var catalogView = app.views.create('#view-catalog', {
+ url: '/catalog/'
+ });
+ var settingsView = app.views.create('#view-settings', {
+ url: '/settings/'
+ });*/
+
+$(document).ready(function () {
+    $$('#formLogin').on('submit', function (e) {
+        e.preventDefault();
+        //formData recebe um objeto com todos os inputs do formulário. Para acessar um determinado valor: formData.nameHTMLdoCampo
+        //formData = app.form.convertToData(this);
+        //console.log(formData);
+        /*$.ajax({
+         type: 'GET',
+         contentType: 'application/json',
+         url: "http://localhost:8080/spge/webresources/usuario/1",
+         dataType: "json",
+         //data: formLoginToJSON(),
+         success: function () {
+         alert('Sucesso');
+         }
+         });*/
+        $.ajax({
+            type: 'GET',
+            url: 'http://localhost:8080/spge/webresources/usuario/1',
+            dataType: "json",
+            success: function (data) {
+                alert('Sucesso');
+            }
+        });
+    });
 });
 
-
-// Login Screen Demo
-$$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
-
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
-
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
-});
+function formLoginToJSON() {
+    return JSON.stringify({
+        "email": $('#email').val(),
+        "senha": $('#senha').val()
+    });
+}
