8dc476d126c76fee9f1fa1cabccb7e54b4977ffb
Tom <tomasjose1201@gmail.com>
2018-05-23 19:17:24 -0300

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -11,19 +11,23 @@ var app = new Framework7({
     data: function () {
         return {
             user: {
-                firstName: 'John',
-                lastName: 'Doe'
+                nome: 'Tomás'
             },
-            // Demo products for Catalog section
             eventos: [
                 {
-                    id: '1'
+                    id: '1',
+                    nome: 'Semana Acadêmica TADS',
+                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit. Etiam efficitur turpis nec libero ornare, et molestie sem tincidunt. Donec egestas elit sit amet lectus consequat, ut tristique sem euismod.'
                 },
                 {
-                    id: '2'
+                    id: '2',
+                    nome: 'Palestra com Dr. Drauzio Varella',
+                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit. Etiam efficitur turpis nec libero ornare, et molestie sem tincidunt. Donec egestas elit sit amet lectus consequat, ut tristique sem euismod.'
                 },
                 {
-                    id: '3'
+                    id: '3',
+                    nome: 'Workshop de HTML5/CSS',
+                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit. Etiam efficitur turpis nec libero ornare, et molestie sem tincidunt. Donec egestas elit sit amet lectus consequat, ut tristique sem euismod.'
                 }
             ]
         };
