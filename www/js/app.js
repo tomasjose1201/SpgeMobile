0b56503dd3c97b4996ba7e3f773e8936e3c423a7
Tom <tomasjose1201@gmail.com>
2018-06-21 23:21:08 -0300

    ...

--- www/js/app.js
+++ www/js/app.js
@@ -10,57 +10,9 @@ var app = new Framework7({
     // App root data
     data: function () {
         return {
+            BASE_URL: 'http://192.168.0.12:8080/spge/webresources/usuario/',
             user: {},
-            eventos: [
-                {
-                    id: '1',
-                    nome: 'Semana Acadêmica TADS',
-                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit.',
-                    organizador: 'Willian Bontorin',
-                    localizacao: 'Av Castro Alves, 13 - Centro, Curitiba, Paraná',
-                    dataHoraInicio: '25/10/2018 19:00',
-                    dataHoraEncerramento: '29/10/2018 22:00',
-                    dataHoraEncerramentoInscricoes: '25/10/2018 12:00',
-                    tipoEvento: 'Público',
-                    numMaxParticipantes: '500',
-                    emiteCertificado: 'Sim',
-                    preco: 'Gratuito',
-                    website: 'http://example.com/',
-                    facebook: 'http://facebook.com/'
-                },
-                {
-                    id: '2',
-                    nome: 'Palestra com Dr. Drauzio Varella',
-                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit.',
-                    organizador: 'Gabriel Schilichta',
-                    localizacao: 'Rua Amazonas, 1780 - Água Verde, Curitiba, Paraná',
-                    dataHoraInicio: '20/07/2018 20:00',
-                    dataHoraEncerramento: '20/07/2018 22:00',
-                    dataHoraEncerramentoInscricoes: '20/07/2018 18:00',
-                    tipoEvento: 'Privado',
-                    numMaxParticipantes: '50',
-                    emiteCertificado: 'Não',
-                    preco: 'R$ 35,00',
-                    website: 'http://example.com/',
-                    facebook: 'http://facebook.com/'
-                },
-                {
-                    id: '3',
-                    nome: 'Workshop de HTML5/CSS',
-                    descricao: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam suscipit volutpat velit vitae auctor. In id facilisis lacus. Donec nec quam in dui dapibus feugiat sit amet a elit.',
-                    organizador: 'Renan Moreira',
-                    localizacao: 'Av João Gualberto, 2001 - Juveve, Curitiba, Paraná',
-                    dataHoraInicio: '02/06/2018 10:00',
-                    dataHoraEncerramento: '02/06/2018 18:00',
-                    dataHoraEncerramentoInscricoes: '01/06/2018 23:59',
-                    tipoEvento: 'Privado',
-                    numMaxParticipantes: '30',
-                    emiteCertificado: 'Sim',
-                    preco: 'R$ 29,90',
-                    website: 'http://example.com/',
-                    facebook: 'http://facebook.com/'
-                }
-            ],
+            eventos: {},
             convidados: [
                 {
                     id: '1',
@@ -127,7 +79,7 @@ var loginView = app.views.create('#view-login', {
 $$('#formLogin').on('submit', function (e) {
     e.preventDefault();
     $.ajax({
-        url: "http://192.168.0.182:8080/spge/webresources/usuario/validar",
+        url: app.data.BASE_URL + "validar",
         type: 'GET',
         data: {
             email: $('#email').val(),
@@ -140,49 +92,37 @@ $$('#formLogin').on('submit', function (e) {
                 app.dialog.alert("Os dados inseridos não foram encontrados.");
             } else {
                 app.data.user = JSON.parse(JSON.stringify(data));
-                
-                var eventosView = app.views.create('#view-eventos', {
-                    url: '/eventos/'
-                });
-                var perfilView = app.views.create('#view-perfil', {
-                    url: '/perfil/'
-                });
+                if (app.data.user.estudante === 'S') {
+                    app.data.user.estudante = true;
+                } else {
+                    app.data.user.estudante = false;
+                }
 
-                var resultadoPesquisa = app.t7.compile(app.$('#itemlist').html());
+                $.ajax({
+                    url: app.data.BASE_URL + "eventos",
+                    type: 'GET',
+                    data: {
+                        idUsuario: app.data.user.idUsuario
+                    },
+                    contentType: 'application/json',
+                    dataType: 'jsonp',
+                    success: function (data) {
+                        app.data.eventos = JSON.parse(JSON.stringify(data));
 
-                function searchEvento(input) {
-                    var encontrou = false;
-                    app.data.eventos.forEach(function (evento) {
-                        if (evento.nome.toUpperCase().includes(input.toUpperCase())) {
-                            app.$("#searchresults").empty().html(resultadoPesquisa(evento));
-                            encontrou = true;
-                        }
-                    });
-                    if (!encontrou) {
-                        app.dialog.alert("Sem resultados.");
+                        var eventosView = app.views.create('#view-eventos', {
+                            url: '/eventos/'
+                        });
+                        var perfilView = app.views.create('#view-perfil', {
+                            url: '/perfil/'
+                        });
+
+                        document.getElementById("tabbar").style.visibility = "visible";
+                        app.router.navigate('/eventos/');
+                    },
+                    error: function (request, textStatus, errorThrown) {
+                        alert(errorThrown + ' Status: ' + textStatus);
                     }
-                }
-
-                var popupListaConvidados;
-                $(document).ready(function () {
-                    $$('#botaoListaConvidados').on('click', function (e) {
-                        console.log("Estou aqui dentro do evento de clique do botão!");
-                        popupListaConvidados = app.popup.create({
-                            content: '<div class="popup">...</div>',
-                            on: {
-                                opened: function () {
-                                    console.log('Popup opened')
-                                }
-                            }
-                        })
-                    });
                 });
-
-                function logout() {
-                    window.location.reload();
-                }
-                document.getElementById("tabbar").style.visibility = "visible";
-                app.router.navigate('/eventos/');
             }
         },
         error: function (request, textStatus, errorThrown) {
@@ -190,3 +130,37 @@ $$('#formLogin').on('submit', function (e) {
         }
     });
 });
+
+var resultadoPesquisa = app.t7.compile(app.$('#itemlist').html());
+
+function searchEvento(input) {
+    var encontrou = false;
+    app.data.eventos.forEach(function (evento) {
+        if (evento.nome.toUpperCase().includes(input.toUpperCase())) {
+            app.$("#searchresults").empty().html(resultadoPesquisa(evento));
+            encontrou = true;
+        }
+    });
+    if (!encontrou) {
+        app.dialog.alert("Sem resultados.");
+    }
+}
+
+var popupListaConvidados;
+$(document).ready(function () {
+    $$('#botaoListaConvidados').on('click', function (e) {
+        console.log("Estou aqui dentro do evento de clique do botão!");
+        popupListaConvidados = app.popup.create({
+            content: '<div class="popup">...</div>',
+            on: {
+                opened: function () {
+                    console.log('Popup opened')
+                }
+            }
+        })
+    });
+});
+
+function logout() {
+    window.location.reload();
+}
